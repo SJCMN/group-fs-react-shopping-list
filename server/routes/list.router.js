c70d58a79ca46e9898ccd7c9d0e18bfb927ce1ff
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-21 11:43:38 -0500

    server GET made, not tested

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    console.log('In server GET');
+    const queryText = `
+        SELECT & FROM list ORDER BY 
+    `
+    pool.query(queryText)
+        .then(response => {
+            console.log('response from DB: ', response) 
+        })
+        .catch(err => {
+            console.log('Error from DB: ', err);
+            res.sendStatus(500)
+        })
+})
 
 module.exports = router;
