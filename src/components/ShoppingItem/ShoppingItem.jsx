eeaeff6523528a4bf51ce636cc101a9d095f4c7f
Nick Allen <rutherford.t.b@gmail.com>
2021-10-21 14:51:31 -0500

    commented out deleteItem() added to ShoppingItem component

--- src/components/ShoppingItem/ShoppingItem.jsx
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -3,16 +3,15 @@ import './ShoppingItem.css';
 function ShoppingItem({item, fetchList}) {
     //const id = item.id // assuming that this component is receiving 'item' as a prop
 
-    const deleteItem = () => {
-        axios.delete(`/list/${id}`).then(result => {
-            console.log('DELETE success');
-            fetchList();
-        }).catch(err => {
-            console.log('DELETE ERR');
-        });
+    // const deleteItem = () => {
+    //     axios.delete(`/list/${id}`).then(result => {
+    //         console.log('DELETE success');
+    //         fetchList();
+    //     }).catch(err => {
+    //         console.log('DELETE ERR');
+    //     });
 
-
-    }
+    // }
 
 
     return 
