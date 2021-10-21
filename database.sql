35db00a72029d6e10155badbd5e6c5eec45de3b2
SJCMN <sam.clavette@gmail.com>
2021-10-21 10:57:42 -0500

    sql file

--- database.sql
+++ database.sql
@@ -1,2 +1,19 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+CREATE TABLE group_shopping_list (
+	id SERIAL PRIMARY KEY,
+	item VARCHAR(80) NOT NULL,
+	quantity FLOAT NOT NULL,
+	unit VARCHAR(20),
+	isPurchased BOOLEAN DEFAULT false
+);
+
+
+INSERT INTO group_shopping_list 
+(item, quantity, unit)
+VALUES
+('bread', 1, 'loaf'),
+('apples', 3, 'each'),
+('pears', 5, 'each');
