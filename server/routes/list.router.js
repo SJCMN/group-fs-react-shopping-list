2a2fab93f9396d4fade614662bc13e1fe175f88b
Nick Allen <rutherford.t.b@gmail.com>
2021-10-21 14:19:16 -0500

    DELETE written

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -11,6 +11,25 @@ const pool = require('../modules/pool.js');
 //PUT
 
 //DELETE
+router.delete('/:id')
+    .then((req, res) => {
+        const id = req.params.id;
+        const queryText = `DELETE FROM list
+            WHERE id = $1;`;
 
+        const values = [id];
+
+        pool.query(queryText, values).then(result => {
+            console.log('Item deleted at id:', id);
+            res.sendStatus(204);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+
+    }).catch(err => {
+        console.log('DELETE ERR', err);
+        res.sendStatus(500);
+    })
 
 module.exports = router;
