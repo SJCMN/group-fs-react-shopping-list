3557f895b3320534537f946aceeb03b81f5f4bef
Eric Meinzer <emeinzer7@gmail.com>
2021-10-21 12:11:39 -0500

    return shopping list rough structure setup

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -3,9 +3,30 @@ import './ShoppingList.css';
 function ShoppingList (){
 
 
-return(
-    console.log('');
-)
+    return(
+        <div className="shopping-list-container" >
+
+            <div className="shopping-list-header">
+                <h2>Shopping List</h2>
+                <button>Reset</button>
+                <button>Clear</button>
+            </div>
+
+            <div className="item-container">
+
+                <div className="item-card">
+                    <h3>item</h3>
+                    <h5>unit</h5>
+                    <h5>quantity</h5>
+
+                    <button className="button-buy">BUY</button>
+                    <button className="button-remove">REMOVE</button>
+                </div>
+
+            </div>
+
+        </div>
+    )
 
 
 }
